9f3af7b7cb80e2ebd0a3898f0deec91a1671bb96
PIPIPIG233666 <pig.priv@gmail.com>
2019-12-31 18:46:49 -0500

    .
    Change-Id: Ibf70252c146f3da2c627a98bdbf4aeea05d03563

--- updates.sh
+++ updates.sh
@@ -32,7 +32,6 @@ repopick -P build/make ${changes[@]}&
 # device/qcom/sepolicy
 changes=(
 264669 # [DNM][SQUASH] Merge tag 'LA.UM.8.1.r1-13000-sm8150.0'
-266117 # msmsteppe: Address init denials
 )
 repopick -P device/qcom/sepolicy ${changes[@]}&
 
